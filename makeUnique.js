70ea43b2cceb9832389023ffbb2a6ef48154d658
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-04-12 10:37:52 -1000

    Second Commit

--- makeUnique.js
+++ makeUnique.js
@@ -1,20 +1,30 @@
 
-// Write your solution below:
-const makeUnique = (string) => {
-    let newStr = "";
-    let arr = string.split("")
-    for(let i = 0; i< string.length; i++){
-        for(let j = i + 1; j < string.length; j++){
-            if (arr[i] === arr[j]){
-                arr.splice(j, 1)
-            }
+// // Write your solution below:
+// const makeUnique = (string) => {
+//     let newStr = "";
+//     let arr = string.split("")
+//     for(let i = 0; i< string.length; i++){
+//         for(let j = i + 1; j < string.length; j++){
+//             if (arr[i] === arr[j]){
+//                 arr.splice(j, 1)
+//             }
+//         }
+//     }
+//     console.log(arr)
+//     newStr = arr.join("");
+//     return newStr;
+// }
+
+const makeUnique = (str) => {
+    let answer = ""
+    for(let i = 0; i < str.length; i++){
+        if(answer.includes(str[i])){
+            continue
+        }else{
+            answer += str[i]
         }
     }
-    console.log(arr)
-    newStr = arr.join("");
-    return newStr;
+    return answer
 }
-
-
     
 console.log(makeUnique('helloworld'))
